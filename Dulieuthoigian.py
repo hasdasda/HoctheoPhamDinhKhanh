113d6d9ff2b263ee15875a4b363dc19eb93d3236
hasdasda <109478759+hasdasda@users.noreply.github.com>
2024-10-22 22:32:19 +0700

    Add files via upload

--- Dulieuthoigian.py
+++ Dulieuthoigian.py
@@ -3,4 +3,9 @@
 from datatime import datetime
 def parser(x):
     # Để biết được định dạng strftime của một chuỗi kí tự ta phải tra trong bảng string format time
-return datetime.strptime
+    return datetime.strptime(x,'%Y-%m-%d %H:%M:%S')
+
+dataset['created'] = dataset['created'].map(lambda x:parser(x))
+#Kiểm tra định dạng time
+for i, k in zip(dataset.columns, dataset.dtypes):
+    print('{}: {}'.format(i,k))
