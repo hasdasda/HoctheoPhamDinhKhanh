c19ca2b3d39e3c102a0a3c6adae894ed3faa49e3
hasdasda <109478759+hasdasda@users.noreply.github.com>
2024-10-26 23:59:18 +0700

    Add files via upload

--- LSTM.py
+++ LSTM.py
@@ -5,6 +5,11 @@ from keras.callbacks import ModelCheckpoint
 from tensorflow.python.keras.utils import np_utils
 import os
 
-filename = '../input/wonderland.txt'
+filename = 'wonderland.txt'
 raw_text = open(filename).read().lower()
+
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+print('number of letters: ', len(char_to_int))
+print(char_to_int)
 
