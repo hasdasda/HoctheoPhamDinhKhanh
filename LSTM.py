e63e2543bd74ef441ff24722fa3c72c2feb39dc9
hasdasda <109478759+hasdasda@users.noreply.github.com>
2024-10-27 23:20:43 +0700

    Add files via upload

--- LSTM.py
+++ LSTM.py
@@ -1,15 +1,20 @@
-import numpy
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM
-from keras.callbacks import ModelCheckpoint
-from tensorflow.python.keras.utils import np_utils
-import os
+# import numpy
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, LSTM
+# from keras.callbacks import ModelCheckpoint
+# from tensorflow.python.keras.utils import np_utils
+# import os
+#
+# filename = 'wonderland.txt'
+# raw_text = open(filename).read().lower()
+#
+# chars = sorted(list(set(raw_text)))
+# char_to_int = dict((c, i) for i, c in enumerate(chars))
+# print('number of letters: ', len(char_to_int))
+# print(char_to_int)
 
-filename = 'wonderland.txt'
-raw_text = open(filename).read().lower()
-
-chars = sorted(list(set(raw_text)))
-char_to_int = dict((c, i) for i, c in enumerate(chars))
-print('number of letters: ', len(char_to_int))
-print(char_to_int)
-
+import string
+#string.ascii_lowercase
+#string.punctuation
+for letter in string.punctuation:
+    print(letter, end=" ")
